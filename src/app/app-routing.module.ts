7385682c27526df554be44834f56e3900455e4dd
Frida <fridaahlstrom@outlook.com>
2021-11-10 18:55:17 +0000

    added city route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/routes/home/home.component';
 import { CitiesComponent } from './components/routes/cities/cities.component';
+import { CityComponent } from './components/routes/city/city.component';
 import { CustomersComponent } from './components/routes/customers/customers.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'city', component: CitiesComponent },
+  { path: 'city', component: CitiesComponent },            //Shows all cities
+  { path: 'city/:name', component: CityComponent },        //Shows specific city
   { path: 'customers', component: CustomersComponent },
+  { path: '**', component: HomeComponent },               // Wildcard / Fallback route
 ];
 
 
