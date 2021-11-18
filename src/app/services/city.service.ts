5aa8dae737dcc3c7af6424419729c6c8d5345f6c
Frida <fridaahlstrom@outlook.com>
2021-11-18 09:52:29 +0000

    added http service

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -1,38 +1,40 @@
 import { Injectable } from '@angular/core';
-import { Observable, of} from "rxjs";
-
+import { Subject, Observable, of} from "rxjs";
+import { HttpService } from './http.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
-  _cities: any = [
-    {
-      "id": 1,
-      "name": "Skövde",
-      "lat_pos": 58.1815656,
-      "lon_pos": 13.9546027
-    },
-    {
-      "id": 2,
-      "name": "Lund",
-      "lat_pos": 55.7048771,
-      "lon_pos": 13.190846
-    },
-    {
-      "id": 3,
-      "name": "Uppsala",
-      "lat_pos": 59.859589,
-      "lon_pos": 17.6363316
-    }
-  ]  
+  private subject = new Subject<any>();
+  _cities: any = [];
 
-  constructor() { }
+  constructor(private httpService: HttpService) { 
+    this.getCities();
+  }
 
   //Will return _cites as an Observable. call cityService.cities | async in order to loop the array
   get cities() {
     return of(this._cities);
+  }
+
+  // setCity(id: any) {
+  //   this.httpService.getCity(id)
+  //   .subscribe((data) => {
+  //     this.subject.next(data)
+  //   })
+  // }
+
+  // onSet(): Observable<any> {
+  //   return this.subject.asObservable();
+  // }
+
+  getCities() {
+    this.httpService.getCities().subscribe((data:any) => {
+      this._cities = data;
+    })
+    return this._cities;
   }
 
 }
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  baseUrl = "http://localhost:8080/api";
+
+  constructor(private http: HttpClient) { }
+
+  getCities() {
+    return this.http.get<any>(`${this.baseUrl}/cities`);
+  }
+
+}
