4e2568664564807d79f4e7e2b4b2b0dd083e0092
Janni Karlsson <jannikarlsson@gmail.com>
2021-11-28 19:27:52 +0000

    Scooter dialog to yellow

--- src/app/components/utils/dialogs/scooter-dialog/scooter-dialog.component.spec.ts
+++ src/app/components/utils/dialogs/scooter-dialog/scooter-dialog.component.spec.ts
@@ -1,15 +1,26 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { StationService } from 'src/app/services/station.service';
 import { ScooterDialogComponent } from './scooter-dialog.component';
 
 describe('ScooterDialogComponent', () => {
   let component: ScooterDialogComponent;
   let fixture: ComponentFixture<ScooterDialogComponent>;
+  let data: any;
+  let stationsStub: any;
 
   beforeEach(async () => {
+    stationsStub = {
+      getStations: () => of([{"id":2,"city_id":1,"location":"Sjukhuset","lat_center":"58.407255","lon_center":"13.824840","radius":"0.002","type":"charge"},{"id":3,"city_id":1,"location":"Circle K","lat_center":"58.390452","lon_center":"13.834545","radius":"0.002","type":"charge"}]),
+      onSet: () => of()
+    }
+    data = {"id":1,"customer_id":null,"city_id":1,"station_id":9,"lat_pos":"58.399560","lon_pos":"13.723922","speed_kph":0,"battery_level":68,"status":"active"}
     await TestBed.configureTestingModule({
       declarations: [ ScooterDialogComponent ],
-      providers: [ MatDialogRef ]
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: MatDialogRef, useValue: {} }, { provide: MAT_DIALOG_DATA, useValue: data }, {provide: StationService, useValue: stationsStub}]
     })
     .compileComponents();
   });
@@ -20,7 +31,9 @@ describe('ScooterDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
 });
