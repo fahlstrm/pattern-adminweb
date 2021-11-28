591feecd423950d9c9d36fac4845a6f90d8e877e
Janni Karlsson <jannikarlsson@gmail.com>
2021-11-28 20:11:36 +0000

    Above 60 per cent

--- src/app/components/tables/customer-table/customer-table.component.spec.ts
+++ src/app/components/tables/customer-table/customer-table.component.spec.ts
@@ -5,12 +5,18 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { CustomerTableComponent } from './customer-table.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CustomerService } from 'src/app/services/customer.service';
+import { of } from 'rxjs';
 
 describe('CustomerTableComponent', () => {
   let component: CustomerTableComponent;
   let fixture: ComponentFixture<CustomerTableComponent>;
+  let customerStub: any;
 
   beforeEach(waitForAsync(() => {
+    customerStub = {
+      getUsers: () => of([{"id":1,"username":"jannikarlsson","token":null,"funds":"200.00","payment_terms":"prepaid"},{"id":2,"username":"fahlstrm","token":null,"funds":"1000.00","payment_terms":"invoice"}])
+    }
     TestBed.configureTestingModule({
       declarations: [ CustomerTableComponent ],
       imports: [
@@ -19,7 +25,8 @@ describe('CustomerTableComponent', () => {
         MatSortModule,
         MatTableModule,
         HttpClientTestingModule
-      ]
+      ],
+      providers: [{provide: CustomerService, useValue: customerStub}]
     }).compileComponents();
   }));
 
@@ -29,7 +36,13 @@ describe('CustomerTableComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should compile', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should log row to console', () => {
+    spyOn(window.console, 'log');
+    component.onClick(1); 
+    expect(window.console.log).toHaveBeenCalled();
+  });
 });
