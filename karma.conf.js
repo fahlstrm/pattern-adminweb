63883ffd5c0807c5e85b2a53f02cbfb627c14166
Frida <fridaahlstrom@outlook.com>
2021-11-29 17:43:33 +0000

    removed chrome sandbox

--- karma.conf.js
+++ karma.conf.js
@@ -41,12 +41,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
     singleRun: false,
     restartOnFileChange: true
   });
