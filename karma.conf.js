32cc2426fa9f96c3689658340b21f9c191731177
Frida <fridaahlstrom@outlook.com>
2021-11-29 17:29:41 +0000

    added chromseHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'CircleCI_ChromeHeadless'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     singleRun: false,
     restartOnFileChange: true
   });
