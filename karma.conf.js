08f65a97dd6324d8e472c03dfce6fb5e75f733e5
Frida <fridaahlstrom@outlook.com>
2021-11-29 19:18:39 +0000

    added type

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
-        [{type: 'lcov'}]
+        { type: 'lcovonly' },
       ]
     },
     reporters: ['progress', 'kjhtml'],
