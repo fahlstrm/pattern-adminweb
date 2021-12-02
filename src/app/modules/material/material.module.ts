37e5c9b71c7c402644cee1e41f39f07d38f0ff0d
Janni Karlsson <jannikarlsson@gmail.com>
2021-12-02 15:23:27 +0000

    Header boxshadow

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -9,7 +9,7 @@ import {MatIconModule} from '@angular/material/icon'; // icon
 import {MatSlideToggleModule} from '@angular/material/slide-toggle'; // toggle
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatProgressBarModule} from '@angular/material/progress-bar'
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 const MaterialComponents = [
     MatButtonModule,
