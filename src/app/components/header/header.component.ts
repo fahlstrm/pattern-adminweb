3b309343e9cde6690bfd0b575a717224819684fa
Frida <fridaahlstrom@outlook.com>
2021-12-03 14:59:45 +0000

    Incorrect grid in app-root

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @HostBinding('class') classes = 'grid grid-wrap';
+
   title = 'Sctr';
   subtitle = 'Admin';
   constructor() {}
--- src/app/components/routes/home/home.component.ts
+++ src/app/components/routes/home/home.component.ts
@@ -3,9 +3,6 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',  
-  // host: {
-  //   class: `grid grid-wrap`
-  // }, //Added to set grid for the router-outlet components
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
