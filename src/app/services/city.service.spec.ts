be07b841d79d58c283fa2a32fb00ed024ec97d44
Janni Karlsson <jannikarlsson@gmail.com>
2021-12-04 12:59:46 +0000

    A few more tests

--- src/app/services/city.service.spec.ts
+++ src/app/services/city.service.spec.ts
@@ -12,7 +12,8 @@ describe('CityService', () => {
   beforeEach(() => {
     cityStub = {
       getCities: () => of([{"id":1,"name":"Sk\u00f6vde","lat_center":"58.394125","lon_center":"13.853491","radius":"5.0"},{"id":2,"name":"Lund","lat_center":"55.710696","lon_center":"13.201312","radius":"2.0"},{"id":3,"name":"Uppsala","lat_center":"59.861534","lon_center":"17.654339","radius":"5.0"}]),
-      loadCities: () => of()
+      loadCities: () => of(),
+      setCity: () => of()
     }
     TestBed.configureTestingModule({
       imports: [
@@ -27,5 +28,12 @@ describe('CityService', () => {
     expect(service).toBeTruthy();
   });
 
-  
+  it('should return cities', () => {
+    let cities = service.getCities();
+    expect(cities).toBeTruthy();
+  });
+
+  it('should return city', () => {
+    expect(service.city).toBeTruthy();
+  });
 });
--- src/app/services/scooter.service.spec.ts
+++ src/app/services/scooter.service.spec.ts
@@ -1,20 +1,29 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { HttpService } from './http.service';
 
 import { ScooterService } from './scooter.service';
+import { StationService } from './station.service';
+
 
 describe('ScooterService', () => {
   let service: ScooterService;
   let scooterStub: any;
+  let stationStub: any;
 
   beforeEach(() => {
+    stationStub = {
+      onSet: () => of({"id":2,"city_id":1,"location":"Sjukhuset","lat_center":"58.407255","lon_center":"13.824840","radius":"0.002","type":"charge"})
+    }
     scooterStub = {
-      getStationScooters: () => of()
+      getStationScooters: () => of([{"id":2,"city_id":1,"location":"Sjukhuset","lat_center":"58.407255","lon_center":"13.824840","radius":"0.002","type":"charge"}]),
+      getCities: () => of(),
+      changeScooterStatus: () => of()
     }
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ]
+      imports: [ HttpClientTestingModule ],
+      providers: [{provide: HttpService, useValue: scooterStub}, {provide: StationService, useValue: stationStub}]
     });
     service = TestBed.inject(ScooterService);
   });
@@ -22,4 +31,27 @@ describe('ScooterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should load scooters', () => {
+    service.city = [1];
+    service.station = {"id": 1};
+    let spy = spyOn(service, 'getStationScooters');
+    service.getStationScooters();
+    expect(spy).toHaveBeenCalled();
+
+  });
+
+  it('should remove scooter from loading', () => {
+    let scooter = {"id":2,"city_id":1,"location":"Sjukhuset","lat_center":"58.407255","lon_center":"13.824840","radius":"0.002","type":"charge"};
+    let spy = spyOn(service, 'changeScooterStatus');
+    service.changeScooterStatus(scooter);
+    expect(spy).toHaveBeenCalled();
+
+  });
+
+  // it('should change scooter status', () => {
+  //   let spy = spyOn(service, 'changeScooterStatus');
+  //   service.changeScooterStatus(1);
+  //   expect(spy).toHaveBeenCalled();
+  // });
 });
